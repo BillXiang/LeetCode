2e541f87b1931856171d5cb8b93dfc8a9e81180b
BillXiang <1373760142@qq.com>
2015-07-17 11:28:05 +0800

    主函数，调用各个问题的测试函数进行测试

---
+++ LeetCode/main.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //twoSumMain();
+    //addTwoNumbersMain();
+    //lengthOfLongestSubstringMain();
+    //findMedianSortedArraysMain();
+    //longestPalindromeMain();
+    zigZagConversionMain();
+    return 0;
+}

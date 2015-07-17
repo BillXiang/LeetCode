564bb1863189058795b57492cb803d747ee97655
BillXiang <1373760142@qq.com>
2015-07-17 11:21:46 +0800

    ZigZag Conversion  The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
    P   A   H   N
    A P L S I I G
    Y   I   R
    
    And then read line by line: "PAHNAPLSIIGYIR"
    
    Write the code that will take a string and make this conversion given a number of rows:
    
    string convert(string text, int nRows);
    
    convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".

---
+++ LeetCode/ZigZagConversion.c
@@ -0,0 +1,73 @@
+﻿#include <stdlib.h>
+/**
+a     g     m     s     y
+b   f h   l n   r t   x z
+c e   i k   o q   u w
+d     j     p     v
+--->
+agmsybfhlnrtxzceikoquwdjpv
+**/
+//竖线部分
+#define VERTICAL_CUR_LEN(curLen, curRow, column) {curLen = curRow + 2*column*(numRows-1);}
+//斜线部分
+#define OBLIQUE_CUR_LEN(curLen, curRow, column) {curLen = 2*(column+1)*(numRows-1)-curRow;}
+char* convert(char* s, int numRows) {
+    if(s==NULL || numRows<=0)
+        return NULL;
+    int sLen = strlen(s);
+    if(sLen<=2 || numRows==1 || numRows>=sLen)
+        return s;
+    char *converted = (char *)malloc(sLen+1);
+    converted[sLen] = '\0';
+    char *convertedP = converted;
+    int curRow = 0/*当前行*/, column = 0/*当前列*/, curLen = 0;
+
+    //curRow==0
+    VERTICAL_CUR_LEN(curLen, curRow, column);
+    while(curLen<sLen)
+    {//第0行
+        *convertedP = s[curLen];
+        convertedP++;
+        column++;
+        VERTICAL_CUR_LEN(curLen, curRow, column);
+    }
+
+    curRow++;
+    while(curRow<numRows-1)
+    {//第1~(numRows-2)行
+        column = 0;
+        VERTICAL_CUR_LEN(curLen, curRow, column);
+        while(curLen<sLen)
+        {
+            *convertedP = s[curLen];
+            convertedP++;
+            OBLIQUE_CUR_LEN(curLen, curRow, column);
+            if(curLen<sLen)
+            {
+                *convertedP = s[curLen];
+                convertedP++;
+                column++;
+                VERTICAL_CUR_LEN(curLen, curRow, column);
+            }
+        }
+        curRow++;
+    }
+    //curRow==numRows-1
+    column = 0;
+    VERTICAL_CUR_LEN(curLen, curRow, column);
+    while(curLen<sLen)
+    {//最后一行
+        *convertedP = s[curLen];
+        convertedP++;
+        column++;
+        VERTICAL_CUR_LEN(curLen, curRow, column);
+    }
+    return converted;
+}
+//测试
+void zigZagConversionMain(void)
+{
+    printf("%s\n", convert(
+                           "abcdefghijklmnopqrstuvwxyz", 25
+                           ));
+}
